5d61b84ddf1fad4f255ac4374f8c8ea4fa9a9f5f
simon-zimmermann@sunblinds-raspi <simon.zimmermann@hs-aalen.de>
2023-06-27 11:25:16 +0200

    added crude error handeling

--- sunblinds.py
+++ sunblinds.py
@@ -17,6 +17,7 @@ GPIO.setup(26, GPIO.OUT)
 
 #lat = 48.837938
 #lon = 10.074317
+#https://openweathermap.org/city/2959927
 city = 2959927
 #obs = owm.weather_at_coords(lat, lon)
 
@@ -117,50 +118,57 @@ def go_down():
 owm = pyowm.OWM(owm_token)
 
 while(True):
-    wea = json.loads(owm.weather_at_id(city).get_weather().to_JSON())
-    date = datetime.datetime.now()
-    sun_altitude = get_altitude(lat, lon, date)
-    sun_azimuth = get_azimuth(lat, lon, date)
-    #x = d - l * math.tan(math.radians(sun_altitude))
-    #print("X-value: %f" % x)
+    try:
+        wea = json.loads(owm.weather_at_id(city).get_weather().to_JSON())
+        date = datetime.datetime.now()
+        sun_altitude = get_altitude(lat, lon, date)
+        sun_azimuth = get_azimuth(lat, lon, date)
+        #x = d - l * math.tan(math.radians(sun_altitude))
+        #print("X-value: %f" % x)
 
-    is_weekday = date.isoweekday() in range(1,6)
-    is_sun_at_window = (sun_azimuth_min < sun_azimuth <= sun_azimuth_max)
-    is_sunup = (sun_altitude > sun_altitude_threshold)
-    is_sunny = wea["clouds"] < clouds_threshold
-    is_hot = wea["temperature"]["temp"] > 273.15 + temperature_threshold
-    is_windy = wea["wind"]["speed"] > wind_threshold or wea["wind"]["gust"] > wind_gust_threshold
+        is_weekday = date.isoweekday() in range(1,6)
+        is_sun_at_window = (sun_azimuth_min < sun_azimuth <= sun_azimuth_max)
+        is_sunup = (sun_altitude > sun_altitude_threshold)
+        is_sunny = wea["clouds"] < clouds_threshold
+        is_hot = wea["temperature"]["temp"] > 273.15 + temperature_threshold
+        is_windy = wea["wind"]["speed"] > wind_threshold or wea["wind"]["gust"] > wind_gust_threshold
 
-    # if the blinds need to be up
-    is_blinds_need_up = \
-            is_windy or \
-            (not is_sunup) or \
-            (not is_weekday)
-    # if we want the blinds to be down
-    is_blinds_want_down = \
-            (is_sunny or is_hot) \
-            and \
-            (is_sunup and is_sun_at_window)
+        # if the blinds need to be up
+        is_blinds_need_up = \
+                is_windy or \
+                (not is_sunup) or \
+                (not is_weekday)
+        # if we want the blinds to be down
+        is_blinds_want_down = \
+                (is_sunny or is_hot) \
+                and \
+                (is_sunup and is_sun_at_window)
 
-    print("================================================================================")
-    print("= %s" % date.strftime("%d/%m/%Y %H:%M:%S"))
-    print("================================================================================")
-    print("Current weather report:")
-    print(json.dumps(wea, indent=2))
-    print("Sun altitude: %f\nSun azimuth: %f" % (sun_altitude, sun_azimuth))
-    print("is_weekday: %s" % is_weekday)
-    print("is_sun_at_window: %s" % is_sun_at_window)
-    print("is_sunup: %s" % is_sunup)
-    print("is_sunny: %s" % is_sunny)
-    print("is_hot: %s" % is_hot)
-    print("is_windy: %s" % is_windy)
-    print("is_blinds_need_up: %s" % is_blinds_need_up)
-    print("is_blinds_want_down: %s" % is_blinds_want_down)
+        print("================================================================================")
+        print("= %s" % date.strftime("%d/%m/%Y %H:%M:%S"))
+        print("================================================================================")
+        print("Current weather report:")
+        print(json.dumps(wea, indent=2))
+        print("Sun altitude: %f\nSun azimuth: %f" % (sun_altitude, sun_azimuth))
+        print("is_weekday: %s" % is_weekday)
+        print("is_sun_at_window: %s" % is_sun_at_window)
+        print("is_sunup: %s" % is_sunup)
+        print("is_sunny: %s" % is_sunny)
+        print("is_hot: %s" % is_hot)
+        print("is_windy: %s" % is_windy)
+        print("is_blinds_need_up: %s" % is_blinds_need_up)
+        print("is_blinds_want_down: %s" % is_blinds_want_down)
 
 
-    if (is_blinds_want_down and not is_blinds_need_up):
-        go_down()
-    else:
+        if (is_blinds_want_down and not is_blinds_need_up):
+            go_down()
+        else:
+            go_up()
+
+        time.sleep(delay)
+    except Exception as e:
+        print("GOT AN ERROR!")
+        print(e)
         go_up()
 
 #up
@@ -186,4 +194,3 @@ while(True):
    #         #move_to(x)
    #         correct(last_move)
 #
-    time.sleep(delay)
