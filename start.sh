25111ae4dab39a2fefafc2db3ba8323f5785d294
simon-zimmermann@sunblinds-raspi <simon.zimmermann@hs-aalen.de>
2023-06-29 14:42:04 +0200

    better screen caption

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 screen -X -S sunblinds quit
 screen -d -m -S sunblinds python3 sunblinds.py
-screen -X caption always "%{rw} * | %H * $LOGNAME | %{bw}%c %D |%{-}%-Lw%{rw}%50>%{rW}%n%f* %t %{-}%+Lw%<" 
+screen -X caption always "%50>%t | gnu-screen | quit: ctrl-a + d | ${LOGNAME}@%H | %c" 
+#screen -X caption always "%{rw} * | %H * $LOGNAME | %{bw}%c %D |%{-}%-Lw%{rw}%50>%{rW}%n%f* %t %{-}%+Lw%<" 
